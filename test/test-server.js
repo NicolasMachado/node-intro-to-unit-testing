a18db32dd6acdde9f40087feb0527583c1927f98
Nicolas Machado <nicoma63@gmail.com>
2017-03-04 12:07:59 +0100

    test

--- test/test-server.js
+++ test/test-server.js
@@ -142,3 +142,60 @@ describe('Shopping List', function() {
       });
   });
 });
+
+describe('/recipes', function () {
+
+  before(function() {
+    return runServer(); 
+  });
+
+  after(function() {
+    return closeServer();
+  });
+  // GET
+  it ('should display all recipes on get', function() {
+    return chai.request(app)
+      .get('/recipes')
+      .then(function (res) {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
+      });
+  });
+  // POST
+  it('should add a recipe on POST', function() {
+    const newItem = {name: 'coffee', ingredients: ["coffee", "milk", "cup"]};
+    return chai.request(app)
+      .post('/recipes')
+      .send(newItem)
+      .then(function(res) {
+        res.should.have.status(201);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.should.include.keys('id', 'name', 'ingredients');
+        res.body.id.should.not.be.null;
+        res.body.should.deep.equal(Object.assign(newItem, {id: res.body.id}));
+      });
+  });
+  // PUT
+  it('should update recipe on PUT', function() {
+    const updateData = {
+      name: 'orange juice',
+      ingredients: ["orange", "juice", "glass"]
+    };
+    return chai.request(app)
+      .get('/recipes')
+      .then(function(res) {
+        updateData.id = res.body[0].id;
+        return chai.request(app)
+          .put(`/recipes/${updateData.id}`)
+          .send(updateData);
+      })
+      .then(function(res) {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.should.deep.equal(updateData);
+      });
+  });
+});
